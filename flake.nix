0038f0380a955bf41c167a375a07d6d8f059f155
Jul Lang <jullanggit@proton.me>
2026-01-26 13:04:23 +0100

    initial

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Jupyter and the kernel
+            jupyter
+            python3Packages.ipykernel
+
+            # Your Python packages for analysis
+            (python3.withPackages (ps: with ps; [
+              numpy
+              pandas
+              matplotlib
+              scikit-learn
+            ]))
+          ];
+
+          # Optional: Automatically start Jupyter Lab when you enter the shell
+          shellHook = ''
+            echo "Jupyter environment ready."
+            echo "To start Jupyter Lab, run: jupyter lab"
+          '';
+        };
+      }
+    );
+}
